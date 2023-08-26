cb50a79abba659f278b0bba756664ccd623249b4
Dallas Lovell <dallas.lovell@gmail.com>
2023-08-26 14:15:59 -0700

    responses to question

--- script.js
+++ script.js
@@ -29,10 +29,33 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   showSlides(slideIndex);
 
-  setInterval(function() {
+  setInterval(function () {
     plusSlides(1);
   }, 3000);
 });
+
+function questionButton() {
+  const responses = [
+    "It is certain	Reply hazy, try again",
+    "Don’t count on it",
+    "It is decidedly so",
+    "Ask again later	My reply is no",
+    "Without a doubt",
+    "Better not tell you now",
+    "My sources say no",
+    "Yes definitely",
+    "Cannot predict now",
+    "Outlook not so good",
+    "You may rely on it",
+    "Concentrate and ask again",
+    "Very doubtful",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+  ];
+}
